4baf3b2ac634dbab59c1d74eb303caf52e10b86d
Mohamed Hafez Mohamed <42019618+mohamed-hafez-mohamed@users.noreply.github.com>
2019-10-26 16:36:53 +0200

    Update I2C.h

--- MCAL/I2C/I2C.h
+++ MCAL/I2C/I2C.h
@@ -13,13 +13,13 @@
 #include "GPIO.h"
 
 //if i use status table
-#define TW_START         0x08 // start has been sent
-#define TW_REP_START     0x10 // repeated start
-#define TW_MT_SLA_W_ACK  0x18 // ( slave address + Write have been sent ) + Ack received from slave
-#define TW_MT_SLA_R_ACK  0x40 // ( slave address + Read have been sent ) + Ack received from slave
-#define TW_MT_DATA_ACK   0x28 // Data byte has been transmitted and ACK has been received from Slave.
-#define TW_MR_DATA_ACK   0x50 // Master received data and send ACK
-#define TW_MR_DATA_NACK  0x58 // MAster received data but don't send ACK
+#define I2C_START         0x08 // start has been sent
+#define I2C_REP_START     0x10 // repeated start
+#define I2C_MT_SLA_W_ACK  0x18 // ( slave address + Write have been sent ) + Ack received from slave
+#define I2C_MT_SLA_R_ACK  0x40 // ( slave address + Read have been sent ) + Ack received from slave
+#define I2C_MT_DATA_ACK   0x28 // Data byte has been transmitted and ACK has been received from Slave.
+#define I2C_MR_DATA_ACK   0x50 // Master received data and send ACK
+#define I2C_MR_DATA_NACK  0x58 // MAster received data but don't send ACK
 
 typedef enum
 {
@@ -27,7 +27,7 @@ typedef enum
 	I2C_RX
 }i2c_status;
 
-void  i2c_init(i2c_status status);
+void  i2c_init();
 
 void  i2c_start(void);
 
@@ -39,5 +39,8 @@ void  i2c_write(uint8 data);
 
 uint8 i2c_read(void);
 
-uint8 i2c_getStatus(void)
+uint8 i2c_reaWithNAck(void);
+
+uint8 i2c_getStatus(void);
+
 #endif /* I2C_H_ */
