d22a4f8b11e055f0874de35766a0cc68d4ca4441
Mohamed Hafez Mohamed <42019618+mohamed-hafez-mohamed@users.noreply.github.com>
2019-10-26 13:33:39 +0200

    Create i2c.c

---
+++ MCAL/I2C/i2c.c
@@ -0,0 +1,51 @@
+/*
+ * I2C.c
+ *
+ * Created: 9/26/2019 09:08:14 ص
+ *  Author: Mohamed Hafez
+ */ 
+
+#include "I2CMA.h"
+
+#define I2C_BUSY (!(ACCESS_REG_8BIT((ICBASE + CONTROL_OFFSET)) & (1 << TWINT))) 
+
+void  i2c_init(i2c_status status)
+{
+	ACCESS_REG_8BIT((ICBASE + STATUS_OFFSET))   = 0x00;
+	ACCESS_REG_8BIT((ICBASE + BITRATE_OFFSET))  = 0x02;
+	ACCESS_REG_8BIT((ICBASE + CONTROL_OFFSET)) |= (SET << TWEN);
+	ACCESS_REG_8BIT((ICBASE + ADDRESS_OFFSET))  = 0b11010000;
+}
+
+void  i2c_start(void)
+{
+	ACCESS_REG_8BIT((ICBASE + CONTROL_OFFSET)) = (SET << TWEN) | (SET << TWINT) | (SET << TWSTA);
+	while(I2C_BUSY);
+}
+
+void  i2c_stop(void)
+{
+	ACCESS_REG_8BIT((ICBASE + CONTROL_OFFSET)) = (SET << TWEN) | (SET << TWINT) | (SET << TWSTO);
+	while(I2C_BUSY);
+}
+
+void  i2c_writeAddress(uint8 adress)
+{
+	ACCESS_REG_8BIT((ICBASE + DATA_OFFSET))  = adress;
+	ACCESS_REG_8BIT((ICBASE + CONTROL_OFFSET)) = (SET << TWEN) | (SET << TWINT);
+	while(I2C_BUSY);
+}
+
+void  i2c_write(uint8 data)
+{
+	ACCESS_REG_8BIT((ICBASE + DATA_OFFSET))  = data;
+	ACCESS_REG_8BIT((ICBASE + CONTROL_OFFSET)) = (SET << TWEN) | (SET << TWINT);
+	while(I2C_BUSY);
+}
+
+uint8 i2c_read(void)
+{
+	ACCESS_REG_8BIT((ICBASE + CONTROL_OFFSET)) = (SET << TWEN) | (SET << TWINT) | (SET << TWEA);
+	while(I2C_BUSY);
+	return ACCESS_REG_8BIT((ICBASE + DATA_OFFSET));
+}
