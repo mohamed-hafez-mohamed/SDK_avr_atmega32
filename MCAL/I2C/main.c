53080523b019b9b29a79ed0ea57f1d0e6ee1268f
Mohamed Hafez Mohamed <42019618+mohamed-hafez-mohamed@users.noreply.github.com>
2019-11-28 15:51:37 +0200

    Add files via upload

--- MCAL/I2C/main.c
+++ MCAL/I2C/main.c
@@ -1,25 +1,24 @@
 /*
- * I2C.c
+ * ICS.c
  *
- * Created: 9/26/2019 08:55:28 ص
+ * Created: 9/26/2019 04:36:07 م
  * Author : Mohamed Hafez
  */ 
 
-
-#include "I2CMA.h"
+#include "I2CS.h"
 #include <avr/delay.h>
-
+uint8 d;
 int main(void)
 {
-	i2c_init();
-	
-	while (1)
-	{
-		i2c_start();
-		i2c_writeAddress(0b00000100);
-		_delay_ms(10); // wait
-		i2c_write(0x07);
-		i2c_stop();
-	}
+    i2c_init();
+	pin_dirc(B0,PIN_OUTPUT);
+    while (1) 
+    {
+		d = i2c_read();
+		if(d == 0x07)
+		{
+			pin_write(B0,HIGH_LEVEL);
+		}
+    }
 }
 
