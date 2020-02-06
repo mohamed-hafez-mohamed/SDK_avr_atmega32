67987bd0a63f65279d6aaa49b5e96a27f2cff690
Mohamed Hafez Mohamed <42019618+mohamed-hafez-mohamed@users.noreply.github.com>
2020-02-06 07:25:16 +0200

    Update adc_cnfg.h

--- MCAL/ADC/adc_cnfg.h
+++ MCAL/ADC/adc_cnfg.h
@@ -1,7 +1,7 @@
-﻿/*
+/*
  * adc_cnfg.h
  *
- * Created: 9/8/2019 10:04:46 م
+ * Created: 9/8/2019 10:04:46 ?
  *  Author: mah_h
  */ 
 
@@ -11,7 +11,7 @@
 
 
 #define VREF           5.0
-#define MAX_LEVEL_NUM  1023.0
+#define MAX_LEVEL_NUM  1024.0
 
 
 #endif /* ADC_CNFG_H_ */
