9909e50e2539f3e9e8fca7b51dd03c844f4b7fd2
Mohamed Hafez Mohamed <42019618+mohamed-hafez-mohamed@users.noreply.github.com>
2020-03-05 02:26:53 +0200

    Update ADC.c

--- MCAL/ADC/ADC.c
+++ MCAL/ADC/ADC.c
@@ -1,44 +1,53 @@
 /*
- * common.h
+ * ADC.c
  *
- * Created: 8/26/2019 09:48:21 ?
- *  Author: Mohamed Hafez
+ * Created: 9/3/2019 09:23:59 ?
+ *  Author: mah_h
  */ 
 
+//#include <avr/interrupt.h>
+#include "ADC.h"
+#include <avr/interrupt.h>
 
-#ifndef COMMON_H_
-#define COMMON_H_
+#define  ADC_BUSY   IS_BIT_CLEAR((ADCBASE + CSRA_OFFSET),ADIF)
+#define  V_IN	    ACCESS_REG_16BIT((ADCBASE + LOW_OFFSET))
 
-//DIFFERENT DATA TYPES FOR DIFFERENR TARGETS
-typedef unsigned char uint8;
-typedef unsigned int  uint16;
-typedef unsigned long uint32;
-typedef unsigned long long uint64;
-typedef  char sint8;
-typedef  int  sint16;
-typedef  long sint32;
-typedef  long long sint64;
-typedef uint8 bool_t;
+static void (*ptr_callBackFun)(uint16);
 
-#define ACCESS_REG_8BIT(REG)         (*(volatile uint8*)REG)
+void adc_init(ADC_cnfg* ptrADC, void(*ptr_func)(uint16 val))
+{
+	ACCESS_REG_8BIT((ADCBASE + CSRA_OFFSET))  = CLEAR_REGISTER;              //clear ADCSRA REG
+	ACCESS_REG_8BIT((ADCBASE + MUX_OFFSET))   = CLEAR_REGISTER;              //clear ADCMUX REG
+	ACCESS_REG_8BIT((ADCBASE + MUX_OFFSET))   |= ((ptrADC->vref)    << REFS0);  //V REFERENCE (REFS0,REFS1) (bit6,bit7) in ADMUX REG
+	ACCESS_REG_8BIT((ADCBASE + CSRA_OFFSET))  |= ((ptrADC->clk)     << ADPS0);  //clock conversion (ADPS0,ADPS1,ADPS2) in ADCSRA REG
+	SET_BIT((ADCBASE + CSRA_OFFSET),ADEN);                                   //SET EN BIT IN ACSRA REG
+	ACCESS_REG_8BIT((ADCBASE + SFIOR_OFFSET)) |= ((ptrADC->mode)    << ADTS0);  //mode (ADTS0,ADTS1,ADTS2)(bit 5,6,7) in SFIOR   REG
+	if((ptrADC->mode) != FREE_RUNNING_MODE)
+	{
+		SET_BIT((ADCBASE + CSRA_OFFSET),ADATE);                              //set trigger mode (ADATE BIT5)in ADCSRA REG
+	}
+	ptr_callBackFun = ptr_func;
+}
 
-#define ACCESS_REG_16BIT(REG)        (*(volatile uint16*)REG)
+uint16 adc_read(channel_selectType channel)
+{
+	ACCESS_REG_8BIT((ADCBASE + MUX_OFFSET))   |= ((channel) << MUX0);
+	SET_BIT((ADCBASE + CSRA_OFFSET),ADSC);                              //start conversion (ADSC BIT 6) at ACSRA REG
+	while((ADC_BUSY)); 
+	(*ptr_callBackFun)(V_IN);                                                 // check (ADC Interrupt Flag BIT 4) in ACSRA REG
+	return V_IN;
+}
 
-#define READ_REG_8BIT(REG)           (*(volatile uint8*)(REG))
+void adc_int(INT_cnfg adcint, channel_selectType channel, void (*ptr_func)(uint16 val))
+{
+	ACCESS_REG_8BIT((ADCBASE + CSRA_OFFSET))  |= ((adcint)  << ADIE);
+	ACCESS_REG_8BIT((ADCBASE + MUX_OFFSET))   |= ((channel) << MUX0);
+	SET_BIT((ADCBASE + CSRA_OFFSET),ADSC);                                     // start conversion for one shoot
+	ptr_callBackFun = ptr_func;							       
+}
 
-#define READ_REG_16BIT(REG)          (*(volatile uint16*)(REG))
-
-#define WRITE_REG_8BIT(REG,Val)      (ACCESS_REG_8BIT(REG)) = (Val);
-
-#define SET_BIT(REG,BIT_NUM)		 (ACCESS_REG_8BIT(REG) |= 1<<(BIT_NUM))
-
-#define CLEAR_BIT(REG,BIT_NUM)       (ACCESS_REG_8BIT(REG) &= (~(1<<(BIT_NUM))))
-
-#define TOGGLE_BIT(REG,BIT_NUM)  	 (ACCESS_REG_8BIT(REG) ^= (1<<BIT_NUM) )
-
-#define IS_BIT_SET(REG,BIT_NUM)      ((ACCESS_REG_8BIT(REG) & (1 << BIT_NUM)))
-
-#define IS_BIT_CLEAR(REG,BIT_NUM)    (!(IS_BIT_SET(REG,BIT_NUM)))
-
-
-#endif /* COMMON_H_ */
+ISR(ADC_vect)
+{
+	sei();
+	(*ptr_callBackFun)(V_IN);
+}
