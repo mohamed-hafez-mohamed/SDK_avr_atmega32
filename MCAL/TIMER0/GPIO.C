59578751cef40b437cdf0215f11e2332c946fe4a
Mohamed Hafez Mohamed <42019618+mohamed-hafez-mohamed@users.noreply.github.com>
2020-03-05 02:33:28 +0200

    Update GPIO.C

--- MCAL/TIMER0/GPIO.C
+++ MCAL/TIMER0/GPIO.C
@@ -1,247 +1,93 @@
 /*
- * GPIO.c
+ * TIMER0.c
  *
- * Created: 8/26/2019 09:51:13 ص
- *  Author: Mohamed Hafez
+ * Created: 9/10/2019 02:20:45 ?
+ *  Author: mah_h
  */ 
 
 #include "GPIO.h"
+#include "TIMER0.h"
 
-//PORTS' FUNCTIONS
-void port_dirc(port_t port,portDirc_t dirc)                                                  //SEPCIFY PORT DIRECTION
+static void (*pfunc_ovi)(void);
+static void (*pfunc_oci)(void);
+
+void timer0_init(timer0_cnfg* ptrtimer0)
 {
-	ACCESS_REG_8BIT((port + DIRC_OFFEST)) = dirc;
-}
-
-uint8 port_read(port_t port)                                                                 //READ FROM PORT
-{
-	return ACCESS_REG_8BIT((port + INTPUT_OFFEST));
-}
-
-void port_write(port_t port ,uint8 val)                                                       //WRITE TO PORT
-{
-	ACCESS_REG_8BIT((port + OUTPUT_OFFEST)) = val;
-}
-
-
-void pin_dirc(uint8 pin,pinDirc_t dirc)                                           //SEPCIFY PIN DIRECTION
-{
-	port_t port;
-	if(pin <= END_PORTA)
+	pin_dirc(T0,PIN_INPUT);
+	ACCESS_REG_8BIT((TIMER0BASE + TIMER0_OFFSET))    = CLEAR_REGISTER;                 //clear TCNT0 REG
+	ACCESS_REG_8BIT((TIMER0BASE + CONTROL0_OFFSET))  = CLEAR_REGISTER;                 //clear TCCR0 REG
+	ACCESS_REG_8BIT((TIMER0BASE + CONTROL0_OFFSET)) |= (ptrtimer0->Timer0_clkSelect << CS00);
+	ACCESS_REG_8BIT((TIMER0BASE + CONTROL0_OFFSET)) |= (ptrtimer0->Timer0_COM       << COM00);
+	switch(ptrtimer0->Timer0_mode)
 	{
-		port = A;
-	}
-	else if(pin >= START_PORTB && pin <= END_PORTB)
-	{
-		pin -= CNFG_PINS_PORTB;
-		port = B;
-	}
-	else if(pin >= START_PORTC && pin <= END_PORTC)
-	{
-		pin -= CNFG_PINS_PORTC;
-		port = C;
-	}
-	else
-	{
-		pin -= CNFG_PINS_PORTD;
-		port = D;
-	}
-	switch(dirc)
-	{
-		case PIN_INPUT :
-		CLEAR_BIT((port + DIRC_OFFEST),pin);
+		case Normal :
+		   ACCESS_REG_8BIT((TIMER0BASE + CONTROL0_OFFSET))   &= ~(SET << WGM01);
+		   ACCESS_REG_8BIT((TIMER0BASE + CONTROL0_OFFSET))   &= ~(SET << WGM00);
 		break;
-		case PIN_OUTPUT :
-		SET_BIT((port + DIRC_OFFEST),pin);
+		case PWM_phase_correct :
+		   ACCESS_REG_8BIT((TIMER0BASE + CONTROL0_OFFSET))   &= ~(SET << WGM01);
+		   ACCESS_REG_8BIT((TIMER0BASE + CONTROL0_OFFSET))   |=  (SET << WGM00);
+		   pin_dirc(OC0,PIN_OUTPUT);
+		break;
+		case CTC :
+		   ACCESS_REG_8BIT((TIMER0BASE + CONTROL0_OFFSET))   |=  (SET << WGM01);
+		   ACCESS_REG_8BIT((TIMER0BASE + CONTROL0_OFFSET))   &= ~(SET << WGM00);
+		   pin_dirc(OC0,PIN_OUTPUT);
+		break;
+		case Fast_PWM :
+		   ACCESS_REG_8BIT((TIMER0BASE + CONTROL0_OFFSET))   |=  (SET << WGM01);
+		   ACCESS_REG_8BIT((TIMER0BASE + CONTROL0_OFFSET))   |=  (SET << WGM00);
+		   pin_dirc(OC0,PIN_OUTPUT);
 		break;
 	}
 }
 
-void pins_dirc(uint8 pin_start, uint8 pin_end, uint8 dirc)                             //SEPCIFY PINS DIRECTION
+uint8 ocr0(uint8 mode, uint8 value)
 {
-	port_t port;
-	if(pin_start <= END_PORTA)
+	switch(mode)
 	{
-		port = A;
-	}
-	else if(pin_start >= START_PORTB && pin_start <= END_PORTB)
-	{
-		pin_start -= CNFG_PINS_PORTB;
-		pin_end   -= CNFG_PINS_PORTB;
-		port = B;
-	}
-	else if(pin_start >= START_PORTC && pin_start < END_PORTC)
-	{
-		pin_start -= CNFG_PINS_PORTC;
-		pin_end   -= CNFG_PINS_PORTC;
-		port = C;
-	}
-	else
-	{
-		pin_start -= CNFG_PINS_PORTD;
-		pin_end   -= CNFG_PINS_PORTD;
-		port = D;
-	}
-	uint8 i;
-	switch(dirc)
-	{
-		case PIN_INPUT :
-		for(i = pin_start;i <= pin_end;i++)
-		{
-			CLEAR_BIT((port + DIRC_OFFEST),i);
-		}
+		case 'w' :
+		   WRITE_REG_8BIT((TIMER0BASE + OUTPUTCOM0_OFFSET), value);
 		break;
-		case PIN_OUTPUT :
-		for(i = pin_start;i <= pin_end;i++)
-		{
-			SET_BIT((port + DIRC_OFFEST),i);
-		}
-		
+		case 'r' :
+		   return ACCESS_REG_8BIT(TIMER0BASE + OUTPUTCOM0_OFFSET);
 		break;
 	}
 }
 
-bool_t pin_read(uint8 pin)                                                             //READ FROM PIN
+uint8 tcnt0(uint8 mode, uint8 value)
 {
-	port_t port;
-	if(pin <= END_PORTA)
+	switch(mode)
 	{
-		port = A;
-	}
-	else if(pin >= START_PORTB && pin <= END_PORTB)
-	{
-		pin -= CNFG_PINS_PORTB;
-		port = B;
-	}
-	else if(pin >= START_PORTC && pin <= END_PORTC)
-	{
-		pin -= CNFG_PINS_PORTC;
-		port = C;
-	}
-	else
-	{
-		pin -= CNFG_PINS_PORTD;
-		port = D;
-	}
-	return (IS_BIT_SET((port + INTPUT_OFFEST),pin));
-}
-
-void pin_write(uint8 pin,logic_status_t logic)                                        //WRITE TO PIN
-{
-	port_t port;
-	if(pin <= END_PORTA)
-	{
-		port = A;
-	}
-	else if(pin >= START_PORTB && pin <= END_PORTB)
-	{
-		pin -= CNFG_PINS_PORTB;
-		port = B;
-	}
-	else if(pin >= START_PORTC && pin <= END_PORTC)
-	{
-		pin -= CNFG_PINS_PORTC;
-		port = C;
-	}
-	else
-	{
-		pin -= CNFG_PINS_PORTD;
-		port = D;
-	}
-	switch(logic)
-	{
-		case HIGH_LEVEL :
-		SET_BIT((port + OUTPUT_OFFEST),pin);
+		case 'w' :
+		   WRITE_REG_8BIT((TIMER0BASE + OUTPUTCOM0_OFFSET), value);
 		break;
-		case LOW_LEVEL :
-		CLEAR_BIT((port + OUTPUT_OFFEST),pin);
+		case 'r' :
+		   return ACCESS_REG_8BIT(TIMER0BASE + TIMER0_OFFSET);
 		break;
 	}
 }
 
-void set_pins(uint8 pin_start, uint8 pin_end)
+void ov_int(TOVFIE0 Timer0_TOIEvoid, void (*ptr_ISR)(void))
 {
-	port_t port;
-	if(pin_start <= END_PORTA)
-	{
-		port = A;
-	}
-	else if(pin_start >= START_PORTB && pin_start <= END_PORTB)
-	{
-		pin_start -= CNFG_PINS_PORTB;
-		pin_end   -= CNFG_PINS_PORTB;
-		port = B;
-	}
-	else if(pin_start >= START_PORTC && pin_start < END_PORTC)
-	{
-		pin_start -= CNFG_PINS_PORTC;
-		pin_end   -= CNFG_PINS_PORTC;
-		port = C;
-	}
-	else
-	{
-		pin_start -= CNFG_PINS_PORTD;
-		pin_end   -= CNFG_PINS_PORTD;
-		port = D;
-	}
-	uint8 i;
-	for(i = pin_start;i <= pin_end;i++)
-	{
-		SET_BIT((port + OUTPUT_OFFEST),i);
-	}
+	ACCESS_REG_8BIT((TIMER0BASE + MASK_OFFSET)) |= (Timer0_TOIEvoid << TOIE0);
+	pfunc_ovi = ptr_ISR;
 }
 
-void clear_pins(uint8 pin_start, uint8 pin_end)
+void oc_int(OCMIE0 Timer0_OCIEvoid, void (*ptr_ISR)(void))
 {
-	port_t port;
-	if(pin_start <= END_PORTA)
-	{
-		port = A;
-	}
-	else if(pin_start >= START_PORTB && pin_start <= END_PORTB)
-	{
-		pin_start -= CNFG_PINS_PORTB;
-		pin_end   -= CNFG_PINS_PORTB;
-		port = B;
-	}
-	else if(pin_start >= START_PORTC && pin_start < END_PORTC)
-	{
-		pin_start -= CNFG_PINS_PORTC;
-		pin_end   -= CNFG_PINS_PORTC;
-		port = C;
-	}
-	else
-	{
-		pin_start -= CNFG_PINS_PORTD;
-		pin_end   -= CNFG_PINS_PORTD;
-		port = D;
-	}
-	uint8 i;
-	for(i = pin_start;i <= pin_end;i++)
-	{
-		CLEAR_BIT((port + OUTPUT_OFFEST),i);
-	}
+	ACCESS_REG_8BIT((TIMER0BASE + MASK_OFFSET)) |= (Timer0_OCIEvoid << OCIE0);
+	pfunc_oci = ptr_ISR;
 }
 
-void uart_cnfg(void)
+ISR(TIMER0_OVF_vect)
 {
-	pin_dirc(D0,PIN_INPUT);
-	pin_dirc(D1,PIN_OUTPUT);
+	sei();
+	(*pfunc_ovi)();
 }
 
-void spi_cnfg_master(void)
+ISR(TIMER0_COMP_vect)
 {
-	
-	pin_dirc(B4,PIN_OUTPUT);       //SS
-	pin_dirc(B5,PIN_OUTPUT);       //MOSi
-	pin_dirc(B6,PIN_INPUT);	       //MISO
-	pin_dirc(B7,PIN_OUTPUT);	   //SCK
-}
-
-void spi_cnfg_Slave(void)
-{
-	pin_dirc(B4,PIN_INPUT);       //SS
-	pin_dirc(B5,PIN_INPUT);       //MOSi
-	pin_dirc(B6,PIN_OUTPUT);	  //MISO
-	pin_dirc(B7,PIN_INPUT);       //sck
+	sei();
+	(*pfunc_oci)();
 }
