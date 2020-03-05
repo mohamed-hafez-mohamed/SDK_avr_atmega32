96bc0d6fd0abe113c55ca8542250e6b503328f78
Mohamed Hafez Mohamed <42019618+mohamed-hafez-mohamed@users.noreply.github.com>
2020-03-05 02:30:18 +0200

    Update UART.c

--- MCAL/UART/UART.c
+++ MCAL/UART/UART.c
@@ -1,21 +1,25 @@
 /*
  * UART.c
  *
- * Created: 9/16/2019 04:17:24 م
+ * Created: 9/16/2019 04:17:24 ?
  *  Author: Mohamed Hafez
  */ 
 
 #include "UART.h"
 
-void  uart_baudrate(uartbaudrate_cnfg baudrate)
-{
-	ACCESS_REG_8BIT((UARTBASE + UBRRL_OFFSET)) = baudrate;            // baud rate setting
-}
+#define WAIT_TO_TRANSMIT  !IS_BIT_SET((UARTBASE + UCSRA_OFFSET),UDRE)
+#define WAIT_TO_RECEIVE   !IS_BIT_SET((UARTBASE + UCSRA_OFFSET),RXC)
+#define DATA              ACCESS_REG_8BIT((UARTBASE + USART_OFFSET))
 
-void  uart_setframe(uartdata_bits databits,uartparity_cnfg parity,uartstopbit_cnfg stopbits)
+static uint8 (*pfunc_TX)(void);
+static void  (*pfunc_RX)(uint8 rx_value);
+static uint8 (*pfunc_UD)(void);
+
+void  uart_init(UART_cnfg* ptr_uart)
 {
+	ACCESS_REG_16BIT((UARTBASE + UBRRL_OFFSET)) = ((ptr_uart)->baudrate);            // baud rate setting
 	ACCESS_REG_8BIT((UARTBASE + UCSRC_OFFSET)) |= (SET << URSEL);
-	switch(databits)
+	switch((ptr_uart)->databits)
 	{
 		//configuration for different transmitted and received bits(5,6,7,8,9)
 		case _5BIT :
@@ -45,20 +49,20 @@ void  uart_setframe(uartdata_bits databits,uartparity_cnfg parity,uartstopbit_cn
 		break;
 	}
 	
-	ACCESS_REG_8BIT((UARTBASE + UCSRC_OFFSET)) |= (parity   << UPM0);//no parity 
-	ACCESS_REG_8BIT((UARTBASE + UCSRC_OFFSET)) |= (stopbits << USBS);//1stop , 
-}
-
-void  uart_init(uartstate_cnfg state,uartoperation_cnfg operation,uartclock_cnfg colck)
-{
-	ACCESS_REG_8BIT((UARTBASE + UCSRC_OFFSET)) |= (operation << UMSEL); //configuration operation (sync or async)
-	ACCESS_REG_8BIT((UARTBASE + UCSRC_OFFSET)) |= (colck     << UCPOL);
-	switch(state)
+	ACCESS_REG_8BIT((UARTBASE + UCSRC_OFFSET)) |= (((ptr_uart)->parity)    << UPM0);//no parity 
+	ACCESS_REG_8BIT((UARTBASE + UCSRC_OFFSET)) |= (((ptr_uart)->stopbits)  << USBS);//1stop , 
+	ACCESS_REG_8BIT((UARTBASE + UCSRC_OFFSET)) |= (((ptr_uart)->operation) << UMSEL); //configuration operation (sync or async)
+	ACCESS_REG_8BIT((UARTBASE + UCSRC_OFFSET)) |= (((ptr_uart)->colck)     << UCPOL);
+	switch((ptr_uart)->state)
 	{
 		case RECEIVE :
-		   ACCESS_REG_8BIT((UARTBASE + UCSRB_OFFSET)) |= (SET << RXEN); // wake up uart for recieve 
+		   ACCESS_REG_8BIT((UARTBASE + UCSRB_OFFSET)) |= (SET << RXEN); // wake up uart for recieve
 		break;
 		case TRANSMIT:
+		   ACCESS_REG_8BIT((UARTBASE + UCSRB_OFFSET)) |= (SET << TXEN); // wake up uart transmit
+		break;
+		case TRANCIVER :
+		   ACCESS_REG_8BIT((UARTBASE + UCSRB_OFFSET)) |= (SET << RXEN); // wake up uart for recieve
 		   ACCESS_REG_8BIT((UARTBASE + UCSRB_OFFSET)) |= (SET << TXEN); // wake up uart transmit
 		break;
 	}
@@ -66,7 +70,7 @@ void  uart_init(uartstate_cnfg state,uartoperation_cnfg operation,uartclock_cnfg
 
 void  uart_transmit(uint8 data,uint8 databits)
 {
-	while (!(ACCESS_REG_8BIT((UARTBASE + UCSRA_OFFSET)) & (1 << UDRE))); // checking if there's still data in Buffer
+	while (WAIT_TO_TRANSMIT); // checking if there's still data in Buffer
 	switch(databits)
 	{
 		case _5BIT :
@@ -83,7 +87,7 @@ void  uart_transmit(uint8 data,uint8 databits)
 
 uint8 uart_receive(uint8 databits)
 {
-	while (!(ACCESS_REG_8BIT((UARTBASE + UCSRA_OFFSET)) & (1 << RXC))); // checking is that recieve completed
+	while (WAIT_TO_RECEIVE); // checking is that recieve completed
 	switch(databits)
 	{
 		case _5BIT :
@@ -96,4 +100,43 @@ uint8 uart_receive(uint8 databits)
 		
 		break;
 	}
+}
+
+void int_tx(uint8 databits, uint8 (*pfunc_tx)( void))
+{
+	SET_BIT((UARTBASE + UCSRB_OFFSET),TXCIE);
+	pfunc_TX = pfunc_tx;
+}
+
+uint8 int_rx(uint8 databits, void (*pfunc_rx)(uint8 rx_value))
+{
+	SET_BIT((UARTBASE + UCSRB_OFFSET),RXCIE);
+	pfunc_RX = pfunc_rx;
+}
+
+void int_ud(uint8 databits, uint8 (*pfunc_ud)(void))
+{
+	SET_BIT((UARTBASE + UCSRB_OFFSET),UDRIE);
+	pfunc_UD = pfunc_ud;
+}
+
+ISR(USART_TXC_vect)
+{
+	sei();
+	WRITE_REG_8BIT((UARTBASE + USART_OFFSET),(*pfunc_TX)());
+}
+
+ISR(USART_RXC_vect)
+{
+	sei();
+	(*pfunc_RX)(DATA);
+}
+
+ISR(USART_UDRE_vect)
+{
+	sei();
+	//uint8 data = (*pfunc_UD)()
+	WRITE_REG_8BIT((UARTBASE + USART_OFFSET),(*pfunc_UD)());
+	CLEAR_BIT((UARTBASE + UCSRB_OFFSET),UDRIE);
+	
 }
